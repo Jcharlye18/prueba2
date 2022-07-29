bc39a488e4135a05dda0f1bf6d68fdcbea1066e0
Jcharlye18 <jc17arlos@gmail.com>
2022-07-28 23:07:28 -0600

    Mi Primer Commit

---
+++ Inicio.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ExperienciaLaboral</title>
+    <link rel="icon" href="icono.png">
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<!--ENCABEZADO-->
+<header>
+    <h1 >MI HOJA DE VIDA</h1>
+</header>
+<body>
+    <!--BARRA DE NAVEGACION-->
+    <nav>
+        <ul>
+            <li> <a href="Inicio.html">Bienvenida</a> </li>
+            <li> <a href="Datos personales.html">Datos personales</a> </li>
+            <li><a href="experiencia.html">Experiencia</a></li>
+            
+        </ul>
+    </nav>
+    <div class="contenedor">
+        <center><img  src="inicio.png" alt="Trulli" width="500" height="333" ></center>
+        
+    </div>
+    <footer>
+       Todos los derechos reservados a Jaime Alonzo Ramos
+    </footer>
+</body>
+</html>
